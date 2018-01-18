2acb56e9a94608e585ced9d199fdca5d25c08a8b
tanya1998 <tanya16108@iiitd.ac.in>
2018-01-18 19:38:12 +0530

    Add files via upload

--- date.c
+++ date.c
@@ -32,6 +32,12 @@
   				}
 				}
 			}
+		if(strcmp(file[1],"-I")==0)
+		{
+			time_t now = time(NULL);
+   			struct tm *t = localtime(&now);
+   			printf( "%d-%d-%d\n", t->tm_year+1900 ,t->tm_mon+1,t->tm_mday);
+		}
 	
 	}
  	return 0;
