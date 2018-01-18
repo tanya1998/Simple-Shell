1b21e8809e916b52bb6b558bcce34f297b168a90
tanya1998 <tanya16108@iiitd.ac.in>
2018-01-18 20:01:29 +0530

    Add files via upload

--- cat.c
+++ cat.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 int main(int argn, char* file[])
 {
-	printf("%d",argn);
  FILE *fp;
  char line[100000];
  char *status;
@@ -13,6 +12,7 @@ int main(int argn, char* file[])
 		exit(0);
 	}
  int i =1;
+if(strcmp(file[1],"-n")!=0 && strcmp(file[1],"-b")!=0 )
  for(;i<argn;i++)
 	{
 		fp = fopen(file[i],"r");
@@ -21,12 +21,66 @@ int main(int argn, char* file[])
 			printf("File Not Found");
 			exit(0);
 		}
+		int count =1;
 		do
 		{
 			status = fgets(line,sizeof(line),fp);
 			printf("%s",line);
+			count++;
 		}
 		while(status);
 		
 	} 
+else
+	{
+	if(argn ==2)
+	printf("More parameters required:- Filename Missing");
+	else if(strcmp(file[1],"-n")==0)
+	{ for(i=2;i<argn;i++)
+	{
+		fp = fopen(file[i],"r");
+		if(fp == NULL)
+		{
+			printf("File Not Found");
+			exit(0);
+		}
+		int count =1;
+		do
+		{
+			status = fgets(line,sizeof(line),fp);
+			
+			printf("%d%s",count,line);
+			count++;
+			
+			
+		}
+		while(status);
+	}
+	}
+	else
+	{
+		for(i=2;i<argn;i++)
+	{
+		fp = fopen(file[i],"r");
+		if(fp == NULL)
+		{
+			printf("File Not Found");
+			exit(0);
+		}
+		int count =1;
+		do
+		{
+			status = fgets(line,sizeof(line),fp);
+			if(strcmp(line,"\n")!=0)
+			{printf("%d%s",count,line);
+			count++;
+			}
+			else
+			{printf("%s",line);
+			}
+		}
+		while(status);
+	}
+	}
+}
 }
